215b1d383e796e8337dbb34f8bc4f480d03f627e
Cesar Miquel <miquel@easytech.com.ar>
2014-06-17 13:49:58 +0000

    Sacamos codigo comentado.

--- docroot/sites/all/modules/custom/jsonblocks/theme/latest-posts.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/latest-posts.tpl.php
@@ -1,12 +1,5 @@
 <div class="latest-posts-block">
 	<div class="inner-block">
-		<!-- <div class="top-txt clearfix">
-			<label>Knight International</label>
-			<label>Media Innovators</label>
-		</div>
-		<div class="txt">
-			<label>An IJNet blog on global news innovation and experimentation.</label>
-		</div> -->
 		<a href="/" class="img-button">READ THE LATEST POSTS</a>
 	</div>
 </div>
