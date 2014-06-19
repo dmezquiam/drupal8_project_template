351c87819d3f70ac05f35f4f45d6fd9cbe44c221
Ana Colautti <ana@easytech.com.ar>
2014-06-19 21:51:21 +0000

    Agregué links a botones social

--- docroot/sites/all/modules/custom/jsonblocks/theme/social-block.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/social-block.tpl.php
@@ -1,10 +1,10 @@
 <div class="social-icons clearfix">
-	<a href="" class="social-icon social-icon-facebook"></a>
-	<a href="" class="social-icon social-icon-twitter"></a>
-	<a href="" class="social-icon social-icon-youtube"></a>
-	<a href="" class="social-icon social-icon-in"></a>
-	<a href="" class="social-icon social-icon-gplus"></a>
-	<a href="" class="social-icon social-icon-rss"></a>
+	<a href="https://www.facebook.com/IJNet" class="social-icon social-icon-facebook" target=_blank></a>
+	<a href="http://twitter.com/ijnet" class="social-icon social-icon-twitter" target=_blank></a>
+	<a href="http://www.youtube.com/ijnetvideo" class="social-icon social-icon-youtube" target=_blank></a>
+	<a href="http://www.linkedin.com/groups/International-Journalists-Network-IJNet-3734352" class="social-icon social-icon-in" target=_blank></a>
+	<a href="https://plus.google.com/109438369213951959896/posts" class="social-icon social-icon-gplus" target=_blank></a>
+	<a href="/rss.xml" class="social-icon social-icon-rss" target=_blank></a>
 </div>
 <div class="mail-subscribe">
 	<div class="dropdown">
