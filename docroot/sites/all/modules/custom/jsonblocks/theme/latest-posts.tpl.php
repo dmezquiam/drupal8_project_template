980c1fb13d63d710785c5ba65b40c9c8d6601337
Cesar Miquel <miquel@easytech.com.ar>
2014-08-05 19:53:41 +0000

    Link Knight fundation button to static page.

--- docroot/sites/all/modules/custom/jsonblocks/theme/latest-posts.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/latest-posts.tpl.php
@@ -1,5 +1,5 @@
 <div class="latest-posts-block">
 	<div class="inner-block">
-		<a href="/" class="img-button"><?php print t('READ THE LATEST POSTS'); ?></a>
+    <a href="<?php url('knight-international-media-innovators') ?>" class="img-button"><?php print t('READ THE LATEST POSTS'); ?></a>
 	</div>
 </div>
