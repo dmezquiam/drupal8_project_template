22f4a16b2b3537e22b903c94b636d61c6194cdc2
Cesar Miquel <miquel@easytech.com.ar>
2014-08-05 21:55:25 +0000

    Fixes to theme

--- docroot/sites/all/themes/ijnet/template.php
+++ docroot/sites/all/themes/ijnet/template.php
@@ -46,81 +46,8 @@ function ijnet_pager_next(&$variables) {
 	return theme_pager_next($variables);
 }
 
-
-/**
- * Preprocess variables for the html template.
- */
-/* -- Delete this line to enable.
-function adaptivetheme_subtheme_preprocess_html(&$vars) {
-  global $theme_key;
-
-  // Two examples of adding custom classes to the body.
-
-  // Add a body class for the active theme name.
-  // $vars['classes_array'][] = drupal_html_class($theme_key);
-
-  // Browser/platform sniff - adds body classes such as ipad, webkit, chrome etc.
-  // $vars['classes_array'][] = css_browser_selector();
-
-}
-// */
-
-
-/**
- * Process variables for the html template.
- */
-/* -- Delete this line if you want to use this function
-function adaptivetheme_subtheme_process_html(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables for the page templates.
- */
-/* -- Delete this line if you want to use these functions
-function adaptivetheme_subtheme_preprocess_page(&$vars) {
-}
-function adaptivetheme_subtheme_process_page(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the node templates.
- */
-/* -- Delete this line if you want to use these functions
-function adaptivetheme_subtheme_preprocess_node(&$vars) {
-}
-function adaptivetheme_subtheme_process_node(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the comment templates.
- */
-/* -- Delete this line if you want to use these functions
-function adaptivetheme_subtheme_preprocess_comment(&$vars) {
-}
-function adaptivetheme_subtheme_process_comment(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the block templates.
- */
-/* -- Delete this line if you want to use these functions
-function adaptivetheme_subtheme_preprocess_block(&$vars) {
-}
-function adaptivetheme_subtheme_process_block(&$vars) {
-}
-// */
-
-function ijnet_theme_pager_first(&$variables) {
-	kpr($variables);
-	die('xxxxxx');
+function ijnet_preprocess_page(&$vars) {
+//  kpr($vars);
 }
 
 function ijnet_preprocess_comment(&$variables) {
