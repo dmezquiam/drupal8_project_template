3de1aa6017da33c6adf154fa9d9aa694b8cdb2ea
Cesar Miquel <miquel@easytech.com.ar>
2014-08-11 20:16:58 +0000

    Make URL absolute and use language prefix.

--- docroot/sites/all/modules/custom/fe_videos/js/fe_videos.js
+++ docroot/sites/all/modules/custom/fe_videos/js/fe_videos.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
   jQuery('.search .button-clean-tiny').click(function(){  
     var dropdown = jQuery('#topic-dropdown option:selected').val();
-    window.location.href='videos/topic/' + dropdown;
+    window.location.href= '/' + Drupal.settings.pathPrefix + 'videos/topic/' + dropdown;
   })
 
 })
