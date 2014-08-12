eae732d680372b186d3ebef135511784a78b9f7c
Ana Colautti <ana@easytech.com.ar>
2014-08-12 15:06:29 +0000

    Redirección del link a una pagina en el sitio que oficia de intermediario con el link original

--- docroot/sites/all/modules/custom/jsonblocks/theme/support-ijnet.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/support-ijnet.tpl.php
@@ -1,6 +1,7 @@
 <div class="support-button">
-	<a href='http://www.icfj.org/support-our-work/give-online' class="support-button">
+	<a href="<?php print url('support-our-work'); ?>" class="support-button">
 	    <div class="support"><?php print t('SUPPORT'); ?></div>
 	    <div class="support-ijnet">IJNET</div>
 	</a>
 </div>
+
