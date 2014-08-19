91f186ff76c32b25abb7ff3d6b37ae8a1f1c2e87
Cesar Miquel <miquel@easytech.com.ar>
2014-08-19 19:53:01 +0000

    If on english site show salesforce contact form.

--- docroot/sites/all/modules/custom/jsonblocks/js/social-block.js
+++ docroot/sites/all/modules/custom/jsonblocks/js/social-block.js
@@ -1,13 +1,18 @@
 jQuery(document).ready (
 	function ($) {
-		$('.dropdown > div').toggleClass('no-js js');
-		$('.dropdown .js .inner-dropdown').hide();
-		$('.clicker').click(function(e) {
-			$('.dropdown .js .inner-dropdown').slideToggle(200);
-			$('.clicker').toggleClass('active');
-			e.stopPropagation();
-		});
+
+      $('.dropdown > div').toggleClass('no-js js');
+      $('.dropdown .js .inner-dropdown').hide();
+      $('.clicker').click(function(e) {
+        var pathArray = window.location.pathname.split( '/' );
+        if (pathArray[1] == 'en') {
+          document.location = '/contact-ijnet';
+        } else {
+          $('.dropdown .js .inner-dropdown').slideToggle(200);
+          $('.clicker').toggleClass('active');
+          e.stopPropagation();
+        }
+      });
 
 	}
-	);
-
+);
