0e54ce83ffffc554a31e397b60e7e4c8af180f13
Ana Colautti <ana@easytech.com.ar>
2014-08-19 19:18:45 +0000

    Link del banner Knight International Media Innovators

--- docroot/sites/all/modules/custom/jsonblocks/theme/latest-posts.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/latest-posts.tpl.php
@@ -1,5 +1,5 @@
 <div class="latest-posts-block">
 	<div class="inner-block">
-    <a href="<?php url('knight-international-media-innovators') ?>" class="img-button"><?php print t('READ THE LATEST POSTS'); ?></a>
+    <a href='<?php print url('knight-media-innovators'); ?>' class="img-button"><?php print t('READ THE LATEST POSTS'); ?></a>
 	</div>
 </div>
