8054c383e9341e3f2204fd61ef970c92f1e961f9
Ana Colautti <ana@easytech.com.ar>
2014-08-25 18:59:27 +0000

    "read more" wasn't translatable.

--- docroot/sites/all/modules/custom/jsonblocks/theme/winship-legacy.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/winship-legacy.tpl.php
@@ -4,7 +4,7 @@
 		<div class="content">
 			<img src="/sites/all/modules/custom/jsonblocks/img/winship_legacy_bg.jpg" alt="">
 			<p><?php print t('The IJNet community honors the memory of ICFJ Founders Tom and Liebe Winship with this important forum. Tom Winship was the renowned editor of The Boston Globe. Liebe wrote the groundbreaking advice column, "Ask Beth." Both remained committed to improving the skills of journalists and engaging audiences in meaningful ways. Through their generosity, we are able to offer you a place to share ideas and experiences. '); ?></p>
-			<a href="http://www.icfj.org/winship-legacy" target="_blank">READ MORE</a>
+			<a href="http://www.icfj.org/winship-legacy" target="_blank"><?php print t('READ MORE')?></a>
 		</div>
 	</div>
 </div>
