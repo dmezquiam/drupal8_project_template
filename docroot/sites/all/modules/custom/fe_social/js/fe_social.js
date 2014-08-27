c1f8946774a0b243c2fabff75621abf495a6e844
Ana Colautti <ana@easytech.com.ar>
2014-08-27 17:47:13 +0000

    updating fe_social so that it doesnt depend on the twitter consumer keys

---
+++ docroot/sites/all/modules/custom/fe_social/js/fe_social.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function () { 
+  var switchTo5x=true; 
+  stLight.options({
+    publisher: "ur-4ce5bdda-7b0a-99b8-92e2ce3127e6", 
+    doNotHash: false, 
+    doNotCopy: flase, 
+    hashAddressBar: false
+  })
+}
+);
