c4136c12fb4dc30b222da93079721f6ea7712817
Ana Colautti <ana@easytech.com.ar>
2014-09-02 20:12:44 +0000

    Pipe between Log in and Register

--- docroot/sites/all/modules/custom/jsonblocks/theme/login-block.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/login-block.tpl.php
@@ -3,6 +3,6 @@
       <label class="sign-reg"><a href='/user'> <?php print t(' SIGN IN '); ?> </a><span><?php print t('OR'); ?></span><a href='/user/register'><?php print t(' REGISTER '); ?></a></label>
   <?php else : ?>
     <div class="logged-block">
-      <label class="sign-reg"><a href='/user'><?php print t(' MY ACCOUNT '); ?></a><span></span><a href='/user/logout'><?php print t(' LOG OUT '); ?></a></label>
+      <label class="sign-reg"><a href='/user'><?php print t(' MY ACCOUNT '); ?></a><span>|</span><a href='/user/logout'><?php print t(' LOG OUT '); ?></a></label>
   <?php endif ?>
 </div>
