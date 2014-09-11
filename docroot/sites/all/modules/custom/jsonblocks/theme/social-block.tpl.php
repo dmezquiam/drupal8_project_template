5ade7543742a5ff88581aabf6d92e66ca1709b76
Rodrigo Echeconea <rodrigo@easytech.com.ar>
2014-09-11 19:07:39 +0000

    Agregada logica para cambiar links de redes sociales en funcion del idioma

--- docroot/sites/all/modules/custom/jsonblocks/theme/social-block.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/social-block.tpl.php
@@ -1,10 +1,7 @@
 <div class="social-icons clearfix">
-	<a href="https://www.facebook.com/IJNet" class="social-icon social-icon-facebook" target=_blank></a>
-	<a href="http://twitter.com/ijnet" class="social-icon social-icon-twitter" target=_blank></a>
-	<a href="http://www.youtube.com/ijnetvideo" class="social-icon social-icon-youtube" target=_blank></a>
-	<a href="http://www.linkedin.com/groups/International-Journalists-Network-IJNet-3734352" class="social-icon social-icon-in" target=_blank></a>
-	<a href="https://plus.google.com/109438369213951959896/posts" class="social-icon social-icon-gplus" target=_blank></a>
-	<a href="/rss.xml" class="social-icon social-icon-rss" target=_blank></a>
+  <?php foreach ($urls as $icon) : ?>
+    <a href="<?php print $icon['url'];?>" class="social-icon <?php print $icon['class'];?>" target="_blank"></a>
+  <?php endforeach ?>
 </div>
 <div class="mail-subscribe">
 	<div class="dropdown">
