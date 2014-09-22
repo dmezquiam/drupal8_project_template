730b1d7cd3873fc897ce5419342646719058e391
Rodrigo Echeconea <rodrigo@easytech.com.ar>
2014-09-22 17:23:25 +0000

    Agrego bloqeu de topics en notas

---
+++ docroot/sites/all/modules/custom/fe_news/theme/topic-block.tpl.php
@@ -0,0 +1,10 @@
+<div class="topic-block">
+  <section class="field-type-taxonomy-term-reference">
+    <h2 class="field-label"><?php print t('TOPIC');?>:&nbsp;</h2>
+    <ul class="field-items">
+      <li>
+      <a href="/<?php print $lang;?>/news/<?php print $tid;?>"><?php print $name;?></a>
+      </li>
+    </ul>
+  </section>
+</div>
