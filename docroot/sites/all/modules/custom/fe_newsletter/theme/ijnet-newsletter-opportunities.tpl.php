05f6b2b8e7770f56cd1e99c03e98fa17e6bd05eb
Ana Colautti <ana@easytech.com.ar>
2014-09-26 14:47:11 +0000

    Fix missing "i" in Opportunities title.

--- docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-opportunities.tpl.php
+++ docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-opportunities.tpl.php
@@ -2,7 +2,7 @@
   <table border="0" cellpadding="0" cellspacing="0" width="600" style="padding-top:30px;">
     <tr>
       <td valign="top" width="600">
-        <p style="padding:35px 20px 15px; margin:0;font-size:22px;font-weight:bold;line-height:26px;color: #bf6c2a;">New Opportunites by Region</p>
+        <p style="padding:35px 20px 15px; margin:0;font-size:22px;font-weight:bold;line-height:26px;color: #bf6c2a;">New Opportunities by Region</p>
       </td>
     </tr>
   </table>
