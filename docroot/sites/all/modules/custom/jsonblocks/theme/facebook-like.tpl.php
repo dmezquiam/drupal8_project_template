94384adb43ef8ed3134048ac3e5eb0ca2b5e785c
Rodrigo Echeconea <rodrigo@easytech.com.ar>
2014-09-29 14:15:17 +0000

    Agrego bloque de facebook por idioma

--- docroot/sites/all/modules/custom/jsonblocks/theme/facebook-like.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/facebook-like.tpl.php
@@ -26,8 +26,8 @@
   </div>
   <div class="view-content">
     <div class="logo">
-      <img src="sites/all/themes/ijnet/images/logo_fb.png" alt=""/> <a href="https://www.facebook.com/IJNet"> <?php print t('International Journalist\'s Network (IJNET)'); ?> </a>
+      <img src="sites/all/themes/ijnet/images/logo_fb.png" alt=""/> <a href="<?php print $url;?>"> <?php print t('International Journalist\'s Network (IJNET)'); ?> </a>
     </div>
-    <div class="fb-like" data-href="https://www.facebook.com/IJNet" data-width="https://www.facebook.com/IJNet" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+    <div class="fb-like" data-href="https://www.facebook.com/IJNet" data-width="<?php print $url;?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
   </div>
 </div>
