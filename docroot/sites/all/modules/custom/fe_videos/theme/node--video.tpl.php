d9c80dcc038c408e87676d1737b591e132409396
Ana Colautti <ana@easytech.com.ar>
2014-10-16 21:27:11 +0000

    fixes in fe_redirect_tags function and node video display

--- docroot/sites/all/modules/custom/fe_videos/theme/node--video.tpl.php
+++ docroot/sites/all/modules/custom/fe_videos/theme/node--video.tpl.php
@@ -81,9 +81,8 @@
  */
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <a href="<?php print $node_url; ?>"><?php print $user_picture; ?>
 
-  <?php if (!$page): ?>
+ <?php if (!$page): ?>
     <div class="content"<?php print $content_attributes; ?>>
       <?php
         // We hide the comments and links now so that we can render them later.
@@ -100,6 +99,7 @@
     <div class="content"<?php print $content_attributes; ?>>
       <?php
         // We hide the comments and links now so that we can render them later.
+        hide($content['field_category']);
         hide($content['comments']);
         hide($content['links']);
         print render($content);
