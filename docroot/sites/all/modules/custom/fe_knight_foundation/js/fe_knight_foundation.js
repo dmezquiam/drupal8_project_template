5666c975525d951a9860c9893959554f29e2e85c
Ana Colautti <ana@easytech.com.ar>
2015-04-07 16:34:55 +0000

    andy's and marcos' changes for the authors box in knights page

--- docroot/sites/all/modules/custom/fe_knight_foundation/js/fe_knight_foundation.js
+++ docroot/sites/all/modules/custom/fe_knight_foundation/js/fe_knight_foundation.js
@@ -4,13 +4,13 @@
     attach: function (context, settings) {
       
       var limit = Drupal.settings.fe_knight_foundation.limit;
-      var authors = ($('.author-tabs-list ul li').length) - 1;
+      var authors = ($('.author-tabs-list ul li').length);
  
       $('.author-tabs-list ul li').slice(limit, authors).hide();
-      $('.author-tabs-list a.show-more').click(function(e) {
+      $('.block-fe-knight-foundation .block-footer a.read-more').click(function(e) {
         e.preventDefault();
         $('.author-tabs-list ul li').slice(limit, authors).show();
-        $('.author-tabs-list ul li').last().hide();
+        $('.block-fe-knight-foundation .block-footer').hide();
       }); 
  
     }
