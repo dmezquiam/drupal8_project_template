417a45d61635292ca76bd6ecd2e7fbddac6a382f
Marcos Mesmer y Rosset <marcos@easytech.com.ar>
2015-06-22 16:02:10 +0000

    Fixes heights for social blocks in front page

--- docroot/sites/all/modules/custom/fe_social/js/fe_social.js
+++ docroot/sites/all/modules/custom/fe_social/js/fe_social.js
@@ -5,41 +5,61 @@
 
     attach: function (context, settings) {
 
-      jQuery(window).load(function($){
-        update_content_styles();
+      jQuery(window).load(function($) {
+        update_content_style();
       });
 
-      $(window).resize(function() {
-        update_content_styles();
+      jQuery(window).resize(function() {
+        update_content_style();
       });
  
     }
 
   };
 
-  function update_content_styles() {
-    var first_wrapper = jQuery('.two-66-33.clearfix > div:nth-child(1)');
-    var second_wrapper = jQuery('.two-66-33.clearfix > div:nth-child(2)');
-    if (first_wrapper.height() > second_wrapper.height()) {
-      second_wrapper.height(first_wrapper.height());
+  function update_content_style() {
+    var main_content_wrapper =
+      jQuery('.two-66-33.clearfix > div:nth-child(1)');
+    
+    var social_content_wrapper =
+      jQuery('.two-66-33.clearfix > div:nth-child(2)');
+    var social_block_wrapper =
+      jQuery('.two-66-33.clearfix .pane-jsonblocks-social-block');
+    var facebook_like_wrapper =
+      jQuery('.two-66-33.clearfix .pane-jsonblocks-facebook-like');
+    var blocks_height =
+      social_block_wrapper.height() + facebook_like_wrapper.height();
+    
+    if (blocks_height < main_content_wrapper.height()) {
+      social_content_wrapper.height(main_content_wrapper.height());
+      jQuery('.two-66-33.clearfix > div:nth-child(2) .region-inner').css('height', 'inherit');
     }
 
     var screen_width = $(window).width();
 
     if (screen_width >= 768) {
-      var social_block_wrapper = jQuery('.two-66-33.clearfix .pane-jsonblocks-social-block');
-      var facebook_like_wrapper = jQuery('.two-66-33.clearfix .pane-jsonblocks-facebook-like');
-      var blocks_height = social_block_wrapper.height() + facebook_like_wrapper.height();
-      var occuped_by_blocks = blocks_height / second_wrapper.height();
+      var social_block_wrapper =
+        jQuery('.two-66-33.clearfix .pane-jsonblocks-social-block');
+      var facebook_like_wrapper =
+        jQuery('.two-66-33.clearfix .pane-jsonblocks-facebook-like');
+      var blocks_height =
+        social_block_wrapper.height() + facebook_like_wrapper.height();
+      var free_space = social_content_wrapper.height() - blocks_height +20;
+      var separator_height = free_space/4;
+ 
+      social_block_wrapper.css('margin-bottom', 0);
+      jQuery('.two-66-33.clearfix > div:nth-child(2) > div section div > h2')
+        .css('margin-bottom', separator_height);
+      jQuery('.two-66-33.clearfix > div:nth-child(2) div.social-icons.clearfix')
+        .css('margin-bottom', separator_height);
+      jQuery('.two-66-33.clearfix > div:nth-child(2) div.mail-subscribe')
+        .css('margin-bottom', separator_height);
+      facebook_like_wrapper.css('margin-bottom', separator_height);
 
-      console.log(occuped_by_blocks);
-      if ((occuped_by_blocks < 0.77)) {
-        jQuery('.two-66-33.clearfix > div:nth-child(2) > div div > h2').css('margin-top', '4%');
-        jQuery('.two-66-33.clearfix > div:nth-child(2) > div div > h2').css('margin-bottom', '0px');
-        jQuery('.two-66-33.clearfix > div:nth-child(2) div.social-icons.clearfix').css('margin-top', '8%');
-        jQuery('.two-66-33.clearfix > div:nth-child(2) div.social-icons.clearfix').css('margin-bottom', '10%');
-        jQuery('.two-66-33.clearfix .panel-separator').css('margin-bottom', '16%');
-      }
+    }
+    else {
+      jQuery('.two-66-33.clearfix > div:nth-child(2) .region-inner').css('height', '');
+      social_content_wrapper.css('height', '');
     }
   }
 
