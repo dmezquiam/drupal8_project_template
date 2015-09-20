13e51edd3932eb29a409512758d443dbfca9418d
Cesar Miquel <miquel@easytech.com.ar>
2015-09-20 11:23:32 +0000

    Add login, logout and registration click link.

---
+++ docroot/sites/all/modules/custom/ijnet_gtm/js/ijnet_gtm.js
@@ -0,0 +1,41 @@
+(function ($) {
+  Drupal.behaviors.jinet_gtm = {
+    attach: function (context, settings) {
+      // Check login URL parameter
+      if (parse('login') == 1) {
+        dataLayer.push({'event': 'Sign in'});
+      }
+
+      // Bind to logout if logged in
+      var $logoutLink = $('[href="/user/logout"]');
+      if ($logoutLink.length) {
+        $logoutLink.click(function() {
+          dataLayer.push({'event': 'Sign out'});
+        });
+      }
+
+      // Bind to registration link
+      var $registerSubmitButton = $('#user-register-form #edit-submit');
+      if ($registerSubmitButton.length) {
+        $registerSubmitButton.click(function() {
+          dataLayer.push({'event': 'Registration'});
+        });
+      }
+    }
+  }
+
+  function parse(val) {
+    var result = "",
+      tmp = [];
+    location.search
+    //.replace ( "?", "" ) 
+    // this is better, there might be a question mark inside
+    .substr(1)
+      .split("&")
+      .forEach(function (item) {
+      tmp = item.split("=");
+      if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+    });
+    return result;
+  }
+})(jQuery)
