8ac01551f0007f8c4ebf648f6ffe15713f990904
Ana Colautti <ana@easytech.com.ar>
2015-10-21 19:24:56 +0000

    add GTM code in page.tpl

--- docroot/sites/all/themes/ijnet/page.tpl.php
+++ docroot/sites/all/themes/ijnet/page.tpl.php
@@ -98,9 +98,16 @@
  * @see adaptivetheme_process_page()
  */
 ?>
+
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NDHNV6" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window, document,'script','dataLayer','GTM-NDHNV6');</script>
+<!-- End Google Tag Manager -->
+
 <div id="page-wrapper">
   <div id="page" class="<?php print $classes; ?>">
-
     <?php if($page['leaderboard']): ?>
       <div id="leaderboard-wrapper">
         <div class="container clearfix">
