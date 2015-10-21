fc7fc58f04932ba00d139cb3372c45896388f8fc
Ana Colautti <ana@easytech.com.ar>
2015-10-21 21:02:21 +0000

    fix issue login redirect to english page

--- docroot/sites/all/modules/custom/jsonblocks/theme/login-block.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/login-block.tpl.php
@@ -1,8 +1,8 @@
   <?php if (!$logged) : ?>
     <div class="log-in-block">
-      <label class="sign-reg"><a href='/user'> <?php print t(' SIGN IN '); ?> </a><span><?php print t('OR'); ?></span><a href='/user/register'><?php print t(' REGISTER '); ?></a></label>
+      <label class="sign-reg"><a href="<?php print url('user'); ?>"> <?php print t(' SIGN IN '); ?> </a><span><?php print t('OR'); ?></span><a href="<?php print url('user/register'); ?>"><?php print t(' REGISTER '); ?></a></label>
   <?php else : ?>
     <div class="logged-block">
-      <label class="sign-reg"><a href='/user'><?php print t(' MY ACCOUNT '); ?></a><span>|</span><a href='/user/logout'><?php print t(' LOG OUT '); ?></a></label>
+      <label class="sign-reg"><a href="<?php print url('user'); ?>"><?php print t(' MY ACCOUNT '); ?></a><span>|</span><a href="<?php print url('user/logout'); ?>"><?php print t(' LOG OUT '); ?></a></label>
   <?php endif ?>
 </div>
