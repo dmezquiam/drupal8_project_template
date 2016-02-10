04b2840c09c97a4c39468d7288b1e9344474e2cb
Ana Colautti <ana@easytech.com.ar>
2016-02-10 23:21:32 +0000

    changed text in knights box

--- docroot/sites/all/modules/custom/jsonblocks/theme/knight-foundation.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/knight-foundation.tpl.php
@@ -1,4 +1,4 @@
 <div class="knight-foundation">
 	<div class="logo"></div>
-	<p><?php print t('ICFJ <a href="http://www.icfj.org/our-work/knight">Knight International Journalism Fellows</a> and their colleagues in the field share the lessons they learn as they reinvent global journalism. Knight International Media Innovators is supported by the <a href="http://knightfoundation.org/">John S. and James L. Knight Foundation</a>.');?></p> 
+	<p><?php print t('<a href="http://www.icfj.org/our-work/knight">ICFJ Knight Fellows</a> and their colleagues in the field share how they are reinventing global journalism. Knight International Media Innovators is supported by the <a href="http://knightfoundation.org/">John S. and James L. Knight Foundation</a>.');?></p> 
 </div>
