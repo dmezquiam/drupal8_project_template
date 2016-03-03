705c3c75df281bbc129119c05a21a283e911c513
Ana Colautti <ana@easytech.com.ar>
2016-03-03 16:53:15 +0000

    update ckeditor library

--- docroot/sites/all/libraries/ckeditor/styles.js
+++ docroot/sites/all/libraries/ckeditor/styles.js
@@ -1,5 +1,5 @@
 ﻿/**
- * Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -83,12 +83,6 @@ CKEDITOR.stylesSet.add( 'default', [
 		name: 'Styled image (left)',
 		element: 'img',
 		attributes: { 'class': 'left' }
-	},
-
-	{
-		name: 'Styled image (center)',
-		element: 'img',
-		attributes: { 'class': 'center' }
 	},
 
 	{
