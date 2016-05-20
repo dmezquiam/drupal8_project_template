b1d74b3a111f36beb64fded9c95bf8d82fb4b0e0
Daniela Duarte <daniela@easytech.com.ar>
2016-05-20 18:43:36 +0000

    Add bylines for videos

--- docroot/sites/all/modules/custom/fe_videos/theme/node--video.tpl.php
+++ docroot/sites/all/modules/custom/fe_videos/theme/node--video.tpl.php
@@ -95,15 +95,20 @@
       <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
     <?php print render($title_suffix); ?>
   <?php else :?>
-    <?php print $date; ?>
     <div class="content"<?php print $content_attributes; ?>>
-      <?php
-        // We hide the comments and links now so that we can render them later.
-        hide($content['field_category']);
-        hide($content['comments']);
-        hide($content['links']);
-        print render($content);
-      ?>
+      <div class="author">
+        <?php if (isset($author)): ?>
+         <?php print $author; ?> |
+        <?php endif ?>
+        <?php print $date; ?>
+      </div>
+        <?php
+          // We hide the comments and links now so that we can render them later.
+          hide($content['field_category']);
+          hide($content['comments']);
+          hide($content['links']);
+          print render($content);
+        ?>
     </div>
   <?php endif ?>
 
