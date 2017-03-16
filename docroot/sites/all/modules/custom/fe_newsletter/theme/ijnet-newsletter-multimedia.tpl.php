c431b3154a2ab9986b0aaad3eb586a70950ad6e2
Ana Colautti <ana@easytech.com.ar>
2017-03-16 18:30:30 +0000

    add link to tpl. rearrange fields. Hide multimedia title in bulletin

--- docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-multimedia.tpl.php
+++ docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-multimedia.tpl.php
@@ -3,15 +3,20 @@
     <tr>
       <td valign="top">
         <h4 style="color:#265775;font-size: 14px; margin:0;padding:0px 0 2px; font-family: verdana, arial,sans-serif;">
-          <?php print($multimedia->title)?></a>
+          <?php //print($multimedia->title)?></a>
         </h4>
-  <?php if (isset($multimedia->body)) : ?>
+   <?php if (isset($multimedia->image)) : ?>
+     <?php print theme('image_style', array('style_name' => 'featured_newsletter_img', 'path' => $multimedia->image)); ?>
+   <?php endif; ?>
+   <?php if (isset($multimedia->body)) : ?>
     <p style="font-family: verdana, arial;  font-size: 12px;">
       <?php print($multimedia->body)?>
     </p>
   <?php endif; ?>
-  <?php if (isset($multimedia->image)) : ?>
-    <?php print theme('image_style', array('style_name' => 'featured_newsletter_img', 'path' => $multimedia->image)); ?>
+  <?php if (isset($multimedia->link_url)) : ?>
+    <a href="<?php print $multimedia->link_url; ?>">
+      <?php isset($multimedia->link_title) ? print $multimedia->link_title : print t("Go to our site"); ?>
+    </a>
   <?php endif; ?>
       </td>
     </tr>
