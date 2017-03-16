d2019c3f4a1a3dfd85bf620fe292b52e214c9414
Ana Colautti <ana@easytech.com.ar>
2017-03-16 18:32:54 +0000

    change title of the comment section in the bulletin

--- docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-comments.tpl.php
+++ docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-comments.tpl.php
@@ -2,7 +2,7 @@
   <table border="0" cellpadding="0" cellspacing="0" width="600" style="padding-top:30px;">
     <tr>
       <td valign="top" width="600">
-        <p style="padding:35px 20px 15px; margin:0;font-size:22px;font-weight:bold;line-height:26px;color: #bf6c2a; font-family: verdana, arial,sans-serif;"><?php print t('Comments');?></p>
+        <p style="padding:35px 20px 15px; margin:0;font-size:22px;font-weight:bold;line-height:26px;color: #bf6c2a; font-family: verdana, arial,sans-serif;"><?php print t('Comment of the week');?></p>
       </td>
     </tr>
   </table>
