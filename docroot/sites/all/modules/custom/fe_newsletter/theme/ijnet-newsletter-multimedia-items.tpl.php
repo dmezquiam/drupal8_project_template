94daf47de9e6476cc4ffab582075f67342a9d18f
Ana Colautti <ana@easytech.com.ar>
2017-04-21 18:59:29 +0000

    fu fields in newsletter

--- docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-multimedia-items.tpl.php
+++ docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-multimedia-items.tpl.php
@@ -3,11 +3,11 @@
     <tr>
       <td valign="top" width="600">
         <p style="padding:35px 20px 15px; margin:0;font-size:22px;font-weight:bold;line-height:26px;color: #bf6c2a; font-family: verdana, arial,sans-serif;"><?php print t('Featured Multimedia');?></p>
+<?php foreach ($multimedia_items as $index => $multimedia) : ?>
+  <?php print theme('ijnet_newsletter_multimedia', array('multimedia' => $multimedia))?>
+<?php endforeach; ?>
       </td>
     </tr>
   </table>
   <!-- // End Body \\ -->
 
-<?php foreach ($multimedia_items as $index => $multimedia) : ?>
-  <?php print theme('ijnet_newsletter_multimedia', array('multimedia' => $multimedia))?>
-<?php endforeach; ?>
