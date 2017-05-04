9a4033194231f04219cf8c0bf8be4787c1774afe
Cesar Miquel <miquel@easytech.com.ar>
2017-05-04 17:29:43 +0000

    remove whitespace for advertisements title.

--- docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-advertisements.tpl.php
+++ docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-advertisements.tpl.php
@@ -4,7 +4,7 @@
 <div class="layout one-col fixed-width" style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);word-wrap:break-word;word-break:break-word">
   <div class="layout__inner" style="border-collapse:collapse;display:table;width:100%;background-color:#ffffff">
     <div class="column" style="text-align:left;color:#575c66;font-size:14px;line-height:21px;font-family:&quot;Open Sans&quot;,sans-serif;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px)">
-      <div style="Margin-left:20px;Margin-right:20px;Margin-top:24px;Margin-bottom:24px">
+      <div style="Margin-left:20px;Margin-right:20px;Margin-top:24px;Margin-bottom:5px">
         <h1 style="Margin-top:0;Margin-bottom:0;font-style:normal;font-weight:normal;color:#1e293d;font-size:22px;line-height:31px"><?php print t('Advertisements'); ?></h1>
       </div>
     </div>
