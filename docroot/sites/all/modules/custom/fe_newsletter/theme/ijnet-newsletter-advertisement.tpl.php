c044eecb5e7781eb548ff20bd0fc30eeb0627ee3
Ana Colautti <ana@easytech.com.ar>
2017-05-11 23:51:54 +0000

    rearrange advertisements according to their amount. Displays 1-4.

--- docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-advertisement.tpl.php
+++ docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-advertisement.tpl.php
@@ -1,8 +1,6 @@
-<div class="column" style="text-align:left;color:#575c66;font-size:14px;line-height:21px;font-family:&quot;Open Sans&quot;,sans-serif;Float:left;max-width:320px;min-width:200px;width:320px;width:calc(72200px - 12000%)">
-  <div style="Margin-left:20px;Margin-right:20px;Margin-top:24px">
-    <h3 style="Margin-top:0;Margin-bottom:12px;font-style:normal;font-weight:normal;color:#1c4761;font-size:16px;line-height:24px"><a style="text-decoration:none;color:#bf6c2a" href="<?php print($advertisement->link)?>" target="_blank"><?php print($advertisement->title)?></a></h3>
-  </div>
-  <div style="Margin-left:20px;Margin-right:20px;Margin-bottom:24px">
-    <p class="size-12" style="Margin-top:0;Margin-bottom:0;font-size:12px;line-height:19px" lang="x-size-12"><?php print($advertisement->summary)?></p>
-  </div>
+<div style="Margin-left:20px;Margin-right:20px;Margin-top:24px">
+  <h3 style="Margin-top:0;Margin-bottom:12px;font-style:normal;font-weight:normal;color:#1c4761;font-size:16px;line-height:24px"><a style="text-decoration:none;color:#bf6c2a" href="<?php print($advertisement->link)?>" target="_blank"><?php print($advertisement->title)?></a></h3>
+</div>
+<div style="Margin-left:20px;Margin-right:20px;Margin-bottom:24px">
+  <p class="size-12" style="Margin-top:0;Margin-bottom:0;font-size:12px;line-height:19px" lang="x-size-12"><?php print($advertisement->summary)?></p>
 </div>
--- docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-advertisements.tpl.php
+++ docroot/sites/all/modules/custom/fe_newsletter/theme/ijnet-newsletter-advertisements.tpl.php
@@ -1,6 +1,4 @@
-
 <?php if (!empty($advertisements) > 0) : ?>
-  <!-- // Begin Body \\ -->
 <div class="layout one-col fixed-width" style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);word-wrap:break-word;word-break:break-word">
   <div class="layout__inner" style="border-collapse:collapse;display:table;width:100%;background-color:#ffffff">
     <div class="column" style="text-align:left;color:#575c66;font-size:14px;line-height:21px;font-family:&quot;Open Sans&quot;,sans-serif;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px)">
@@ -11,14 +9,55 @@
   </div>
 </div>
 
-<div class="layout three-col fixed-width" style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);word-wrap:break-word;word-break:break-word">
+  <?php if (sizeof($advertisements) == 1) : ?>
+
+<div class="layout one-col fixed-width" style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);word-wrap:break-word;word-break:break-word">
   <div class="layout__inner" style="border-collapse:collapse;display:table;width:100%;background-color:#ffffff">
   <?php foreach ($advertisements as $index => $advertisement) : ?>
-    <?php if ($index > 3) { break; } ?>
+    <div class="column" style="text-align:left;color:#575c66;font-size:14px;line-height:21px;font-family:&quot;Open Sans&quot;,sans-serif;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px)">
     <?php print theme('ijnet_newsletter_advertisement', array('advertisement' => $advertisements[$index])) ?>
+    </div>
   <?php endforeach; ?>
   </div>
 </div>
+  <?php endif; ?>
+  <?php if ((sizeof($advertisements)%2 == 0) or (sizeof($advertisements) >= 4)) : ?>
+    <?php foreach ($advertisements as $index => $advertisement) : ?>
+      <?php if ($index >= 4) { break; } ?>
+      <?php if (($index % 2) == 0) : ?>
+
+<!-- // Begin Body \\ -->
+<div class="layout two-col fixed-width" style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);word-wrap:break-word;word-break:break-word">
+  <div class="layout__inner" style="border-collapse:collapse;display:table;width:100%;background-color:#ffffff">
+
+      <?php endif; ?>
+
+    <div class="column" style="text-align:left;color:#575c66;font-size:14px;line-height:21px;font-family:&quot;Open Sans&quot;,sans-serif;Float:left;max-width:320px;min-width:300px;width:320px;width:calc(12300px - 2000%)">
+
+      <?php print theme('ijnet_newsletter_advertisement', array('advertisement' => $advertisements[$index])) ?>
+
+    </div>
+      <?php if (($index % 2) == 1 ) : ?>
+  </div>
+</div>
+<!--<div style="line-height:5px;font-size:5px">&nbsp;</div>-->
+
+      <?php endif; ?>
+    <?php endforeach; ?>
+  <?php endif; ?>
+  <?php if (sizeof($advertisements) == 3) : ?>
+
+<div class="layout three-col fixed-width" style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);word-wrap:break-word;word-break:break-word">
+  <div class="layout__inner" style="border-collapse:collapse;display:table;width:100%;background-color:#ffffff">
+  <?php foreach ($advertisements as $index => $advertisement) : ?>
+    <div class="column" style="text-align:left;color:#575c66;font-size:14px;line-height:21px;font-family:&quot;Open Sans&quot;,sans-serif;Float:left;max-width:320px;min-width:200px;width:320px;width:calc(72200px - 12000%)">
+    <?php print theme('ijnet_newsletter_advertisement', array('advertisement' => $advertisements[$index])) ?>
+    </div>
+  <?php endforeach; ?>
+  </div>
+</div>
+  <?php endif; ?>
+
   <!-- // End Body \\ -->
 <?php endif; ?>
 <div style="line-height:30px;font-size:30px">&nbsp;</div>
