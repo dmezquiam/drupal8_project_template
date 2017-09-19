8386d23178bcc2b4a4293d16f77ee24eaefeb15b
Ana Colautti <ana@easytech.com.ar>
2017-09-19 12:08:06 -0300

    add another link to the latest-posts box in the homepage

--- docroot/sites/all/modules/custom/jsonblocks/theme/latest-posts.tpl.php
+++ docroot/sites/all/modules/custom/jsonblocks/theme/latest-posts.tpl.php
@@ -1,5 +1,8 @@
 <div class="latest-posts-block">
 	<div class="inner-block">
-    <a href='<?php print url('knight-media-innovators'); ?>' class="img-button"><?php print t('READ THE LATEST POSTS'); ?></a>
+    <a href="<?php print $knights_page_url; ?>" class="img-button"><?php print $knights_page_title; ?></a>
+    <?php if (isset($knights_tool_url)) : ?>
+    <a href="<?php print $knights_tool_url; ?>" class="img-button"><?php print $knights_tool_title; ?></a>
+    <?php endif; ?>
 	</div>
 </div>
